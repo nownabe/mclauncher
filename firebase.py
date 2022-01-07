4ccdcf9ec065ab1c7a7c623309f6e3dc9258d908
nownabe <1286807+nownabe@users.noreply.github.com>
2022-01-07 06:24:31 +0000

    Refactor firebase

--- firebase.py
+++ firebase.py
@@ -7,21 +7,15 @@ from typing import Callable
 from firebase_admin import credentials, initialize_app, App, firestore
 
 
-def initialize_firebase() -> App:
-    certificate_json = os.environ["FIREBASE_CREDENTIALS_JSON"]
-    certificate = json.loads(certificate_json)
-    cred = credentials.Certificate(certificate)
-    return initialize_app(cred)
+_certificate_json = os.environ["FIREBASE_CREDENTIALS_JSON"]
+_certificate = json.loads(_certificate_json)
+_cred = credentials.Certificate(_certificate)
+app = initialize_app(_cred)
 
+_firestore_client = firestore.client()
 
-def is_authorized_user() -> Callable[[str], bool]:
-    '''Returns the function authorize users.'''
-    firestore_client = firestore.client()
-
-    def _is_authorized_user(email: str):
-        authorized_users_ref = firestore_client.collection('authorized_users')
-        return any(
-            user.get('email') == email for user in authorized_users_ref.stream()
-        )
-
-    return _is_authorized_user
+def is_authorized_user(email: str) -> bool:
+    authorized_users_ref = _firestore_client.collection('authorized_users')
+    return any(
+        user.get('email') == email for user in authorized_users_ref.stream()
+    )
